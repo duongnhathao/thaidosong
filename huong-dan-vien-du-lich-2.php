24974af1f7b0ed7ed37f48d56c2e6b2c5ec2b24b
NhatHao <duongnhathao001@gmail.com>
2020-12-30 03:36:35 +0700

    message

--- huong-dan-vien-du-lich-2.php
+++ huong-dan-vien-du-lich-2.php
@@ -237,6 +237,33 @@
 
     gtag('config', 'UA-23581568-13');
 </script>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function () {
+        FB.init({
+            xfbml: true,
+            version: 'v9.0'
+        });
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="100976415196130"
+     theme_color="#a695c7"
+     logged_in_greeting="TDTU - Ba chấm - Thái độ sống 3 : Chào bạn"
+     logged_out_greeting="TDTU - Ba chấm - Thái độ sống 3 : Chào bạn">
+</div>
 </body>
 
 <!-- Mirrored from preview.colorlib.com/theme/startup2/single-blog.php by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Dec 2020 14:19:11 GMT -->
