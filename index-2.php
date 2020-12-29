26a86e4493ffc75d2c309ad74397d6dc9d8fb786
NhatHao <duongnhathao001@gmail.com>
2020-12-30 03:31:02 +0700

    message

--- index-2.php
+++ index-2.php
@@ -39,57 +39,7 @@
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
     your browser</a> to improve your experience and security.</p>
 <![endif]-->
-
-<header>
-    <div class="header-area ">
-        <div id="sticky-header" class="main-header-area">
-            <div class="container-fluid p-0">
-                <div class="row align-items-center no-gutters">
-                    <div class="col-xl-2 col-lg-2">
-                        <div class="logo-img">
-                            <a href="index-2.php">
-                                <img src="img/logo.png" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-xl-8 col-lg-8">
-                        <div class="main-menu  d-none d-lg-block text-center">
-                            <nav>
-                                <ul id="navigation">
-                                    <li><a class="active" href="index-2.php">Trang chủ</a></li>
-                                    <li><a href="service.php">services</a></li>
-                                    <li><a href="Portfolio.php">Portfolio</a></li>
-                                    <li><a href="#">pages <i class="ti-angle-down"></i></a>
-                                        <ul class="submenu">
-                                            <li><a href="portfolio_details.php">Portfolio details</a></li>
-                                            <li><a href="about.php">about</a></li>
-                                            <li><a href="elements.php">elements</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#">blog <i class="ti-angle-down"></i></a>
-                                        <ul class="submenu">
-                                            <li><a href="blog.php">blog</a></li>
-                                            <li><a href="single-blog.php">single-blog</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="contact.php">Contact</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 d-none d-lg-block">
-                        <div class="log_chat_area d-flex align-items-end">
-                            <a href="#" data-scroll-nav="0" class="say_hi">Nhóm : Ba chấm</a>
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <div class="mobile_menu d-block d-lg-none"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
+<?php include "header.php"?>
 
 
 <div class="slider_area">
@@ -642,5 +592,4 @@
 
 <!-- Mirrored from preview.colorlib.com/theme/startup2/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Dec 2020 14:18:40 GMT -->
 </html>
-layoutdichvu
 
