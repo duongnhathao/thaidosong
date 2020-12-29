360ca062b867ec94b8a4791e268e7798c2a818a3
NhatHao <duongnhathao001@gmail.com>
2020-12-30 02:11:00 +0700

    message

--- san-pham-du-lich.php
+++ san-pham-du-lich.php
@@ -7,15 +7,16 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="shortcut icon" href="img/portfolio/icon.png">
 
-    <title>Du lịch là gì ? | Life Skills - Ba Chấm Team</title>
+    <title>Sản phẩm du lịch | Life Skills - Ba Chấm Team</title>
     <meta name="description" content="Du lịch là gì ?">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Life Skills: Du lịch là gì ?">
     <meta property="og:site_name" content="Life Skills: Du lịch là gì ?">
     <meta property="og:type" content="website">
     <meta property="og:title" content="Life Skills: Du lịch là gì ?">
-    <meta property="og:description" content="Hiệp hội lữ hành quốc tế đã công nhận du lịch là một ngành kinh tế lớn nhất thế giới vượt lên cả ngành sản xuất ô tô, thép điện tử và nông nghiệp. Vì vậy, du lịch đã trở thành một ngành kinh tế mũi nhọn của nhiều quốc gia trên thế giới.">
-    <meta property="og:image" content="img/blog/dulichlagi/anhbia.jpg">
+    <meta property="og:description"
+          content="Hiệp hội lữ hành quốc tế đã công nhận du lịch là một ngành kinh tế lớn nhất thế giới vượt lên cả ngành sản xuất ô tô, thép điện tử và nông nghiệp. Vì vậy, du lịch đã trở thành một ngành kinh tế mũi nhọn của nhiều quốc gia trên thế giới.">
+    <meta property="og:image" content="img/blog/dulichlagi/sanphamdulichj.jpg">
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
     <meta name="twitter:title" content="Life Skills: Du lịch là gì ?">
@@ -40,14 +41,14 @@
     your browser</a> to improve your experience and security.</p>
 <![endif]-->
 
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 
-<div class="bradcam_area breadcam_bg_dulichlagi">
+<div class="bradcam_area breadcam_bg_sanphamdulich">
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
                 <div class="bradcam_text">
-                    <h3>Du lịch là gì ?</h3>
+                    <h3>Sản phẩm du lịch ?</h3>
                 </div>
             </div>
         </div>
@@ -61,42 +62,43 @@
             <div class="col-lg-8 posts-list">
                 <div class="single-post">
                     <div class="feature-img">
-                        <img class="img-fluid" src="img/blog/dulichlagi/anhbia.jpg" alt="Ảnh bìa">
+                        <img class="img-fluid" src="img/blog/dulichlagi/sanphamdulichj.jpg" alt="Ảnh bìa">
                     </div>
                     <div class="blog_details">
-                        <h2>Ngày nay du lịch trở thành một hiện tượng kinh tế xã hội phổ biến.
+                        <h2>Sản phẩm du lịch cũng là một dạng sản phẩm nhằm thoả mãn nhu cầu du lịch của con người.
                         </h2>
                         <ul class="blog-info-link mt-3 mb-4">
-                            <li><a href="#"><i class="fa fa-user"></i> Travel, Lifestyle</a></li>
-                            <li><a href="#"><i class="fa fa-comments"></i> 03 Comments</a></li>
+                            <li><a href="#"><i class="fa fa-user"></i> Travel, Lifestyle , Life Skills</a></li>
                         </ul>
                         <p class="excert">
-                            Hiệp hội lữ hành quốc tế đã công nhận du lịch là một ngành kinh tế lớn nhất thế giới vượt lên cả ngành sản xuất ô tô, thép điện tử và nông nghiệp. Vì vậy, du lịch đã trở thành một ngành kinh tế mũi nhọn của nhiều quốc gia trên thế giới.
+                            Nhu cầu du lịch là sự mong muốn của con người đi đến một nơi khác với nơi ở của mình để có
+                            được những xúc cảm mới, trải nghiệm mới, hiểu biết mới, để phát triển các mối quan hệ xã
+                            hội, phục hồi sức khoẻ, tạo sự thoải mái dễ chịu về tinh thần.
                         </p>
+
                         <p>
-                            Thuật ngữ du lịch đã trở nên khá thông dụng, nó bắt nguồn từ tiếng Hy Lạp với ý nghĩa là đi một vòng. Du lịch gắn liền với nghỉ ngơi, giải trí, tuy nhiên do hoàn cảnh, thời gian và khu vực khác nhau, dưới mỗi góc độ nghiên cứu khác nhau nên khái niệm du lịch cũng không giống nhau.Luật Du lịch Việt Nam 2005 đã đưa ra khái niệm như sau:
-                        </p>
-                        <div class="quote-wrapper">
-                            <div class="quotes">
-                                “Du lịch là các hoạt động liên quan đến chuyến đi của con người ngoài nơi cư trú thường xuyên của mình nhằm đáp ứng nhu cầu tham quan, tìm hiểu, giải trí, nghỉ dưỡng trong một khoảng thời gian nhất định”.
-                            </div>
+                            Nhu cầu của khách du lịch: những mong muốn cụ thể của khách du lịch trong chuyến du lịch cụ
+                            thể.Theo nghĩa rộng: Từ giác độ thỏa mãn chung của sản phẩm du lịch. Sản phẩm du lịch là sự
+                            kết hợp các dịch vụ hàng hóa cung cấp cho du khách, được tạo nên bởi sự kết hợp của việc
+                            khai thác các yếu tố tự nhiên, xã hội với việc sử dụng các nguồn lực: cơ sở vật chất kỹ
+                            thuật và nhân lực tại một cơ sở, một vùng, địa phương hay của một quốc gia.Theo nghĩa hẹp:
+                            Từ giác độ thỏa mãn đơn lẻ từng nhu cầu đi du lịch. Sản phẩm du lịch là sản phẩm hàng hóa cụ
+                            thể thỏa mãn các nhu cầu khi đi du lịch của con người. Có nghĩa là bất cứ cái gì có thể mang
+                            ra trao đổi để thỏa mãn mong muốn của khách du lịch. Bao gồm sản phẩm hữu hình và sản phẩm
+                            vô hình. </p>
+                        <div class="feature-img">
+                            <img class="img-fluid"
+                                 src="img/blog/dulichlagi/kinh-nghiem-cho-be-di-du-lich-thai-lan-2.jpg" alt="Ảnh bìa">
                         </div>
                         <p>
-                            Theo liên hiệp Quốc các tổ chức lữ hành chính thức( International Union of Official Travel Oragnization: IUOTO): Du lịch được hiểu là hành động du hành đến một nơi khác với điạ điểm cư trú thường xuyên cuả mình nhằm mục đích không phải để làm ăn, tức không phải để làm một nghề hay một việc kiếm tiền sinh sống...
-                        </p>
+                            Theo quan điểm Marketting: "Sản phẩm du lịch là những hàng hoá và dịch vụ có thể thoả mãn
+                            nhu cầu của khách du lịch, mà các doanh nghiệp du lịch đưa ra chào bán trên thị trường, với
+                            mục đích thu hút sự chú ý mua sắm và tiêu dùng của khách du lịch". </p>
                         <p>
-                            Tại hội nghị LHQ về du lịch họp tại Roma - Italia ( 21/8 – 5/9/1963), các chuyên gia đưa ra định nghĩa về du lịch: Du lịch là tổng hợp các mối quan hệ, hiện tượng và các hoạt động kinh tế bắt nguồn từ các cuộc hành trình và lưu trú cuả cá nhân hay tập thể ở bên ngoài nơi ở thường xuyên cuả họ hay ngoài nước họ với mục đích hoà bình. Nơi họ đến lưu trú không phải là nơi làm việc cuả họ. Theo các nhà du lịch Trung Quốc: họat động du lịch là tổng hoà hàng loạt quan hệ và hiện tượng lấy sự tồn tại và phát triển kinh tế, xã hội nhất định làm 7 cơ sở, lấy chủ thể du lịch, khách thể du lịch và trung gian du lịch làm điều kiện.
-                        </p>
-                        <div class="feature-img py-5">
-                            <img class="img-fluid" src="img/blog/dulichlagi/du-lich-chau-au-thang-10-1.jpg" alt="Ảnh bìa">
-                        </div>
-                        <p>
-                            Theo I.I pirôgionic, 1985: Du lịch là một dạng hoạt động cuả dân cư trong thời gian rỗi liên quan với sự di chuyển và lưu lại tạm thời bên ngoài nơi cư trú thường xuyên nhằm nghĩ ngơi, chữa bệnh, phát triển thể chất và tinh thần, nâng cao trình độ nhận thức văn hoá hoặc thể thao kèm theo việc tiêu thụ những giá trị về tự nhiên, kinh tế và văn hoá.
-                        </p>
-                        <p>Theo nhà kinh tế học người Áo Josep Stander nhìn từ góc độ du khách: khách du lịch là loại khách đi theo ý thích ngoài nơi cư trú thường xuyên để thoả mãn sinh họat cao cấp mà không theo đuổi mục đích kinh tế.Nhìn từ góc độ thay đổi về không gian cuả du khách: du lịch là một trong những hình thức di chuyển tạm thời từ một vùng này sang một vùng khác, từ một nước này sang một nước khác mà không thay đổi nơi cư trú hay nơi làm việc.</p>
-                        <p>Nhìn từ góc độ kinh tế: Du lịch là một ngành kinh tế, dịch vụ có nhiệm vụ phục vụ cho nhu cầu tham quan giải trí nghĩ ngơi, có hoặc không kết hợp với các hoạt động chữa bệnh, thể thao, nghiên cứu khoa học và các nhu cầu khác. Theo Bản chất du lịch Nhìn từ góc độ nhu cầu của du khách: Du lịch là một sản phẩm tất yếu của sự phát triển kinh tế - xã hội của loài người đến một giai đoạn phát triển nhất định. chỉ trong hoàn cảnh kinh tế thị trường phát triển, gia tăng thu nhập bình quân đầu người, tăng thời gian rỗi do tiến bộ cuả khoa học - công nghệ, phương tiện giao thông và thông tin ngày càng phát triển, làm phát sinh nhu cầu nghĩ ngơi, tham quan du lịch cuả con người. Bản chất đích thực của du lịch là du ngoạn để cảm nhận những giá trị vật chất và tinh thần có tính văn hoá cao.</p>
-                        <p>Xét từ góc độ các quốc sách phát triển du lịch: Dựa trên nền tảng của tài nguyên du lịch để hoạch định chiến lược phát triển du lịch, định hướng các kế hoạch dài hạn, trung hạn và ngắn hạn. lựa chọn các sản phẩm du lịch độc đáo và đặc trưng từ nguốn nguyên liệu trên, đồng thời xác định phương hướng qui hoạch xây dựng cơ sở vật chất - kỹ thuật và cơ sở hạ tầng dịch vụ du lịch tương 8 ứng.Xét từ góc độ sản phẩm du lịch: sản phẩm đặc trưng cuả du lịch là các chương trình du lịch, nội dung chủ yếu cuả nó là sự liên kết những di tích lịch sử, di tích văn hoá và cảnh quan thiên nhiên nổi tiếng cùng với cơ sở vật chất - kỹ thuật như cơ sở lưu trú, ăn uống, vận chuyển.</p>
-                        <p>Xét từ góc độ thị trường du lịch: Mục đích chủ yếu của các nhà tiếp thị du lịch là tìm kiếm thị trường du lịch, tìm kiếm nhu cầu cuả du khách để “ mua chương trình du lịch”. </p>
+                            Theo Điều 4 chương I - Luật Du lịch Việt Nam năm 2005 giải thích từ ngữ: “Sản phẩm du lịch
+                            (tourist product) là tập hợp các dịch vụ cần thiết để thoả mãn nhu cầu của khách du lịch
+                            trong chuyến đi du lịch”.</p>
+
                     </div>
                 </div>
                 <div class="navigation-top">
@@ -132,7 +134,7 @@
                                 </div>
                                 <div class="thumb">
                                     <a href="san-pham-du-lich.php">
-                                        <img class="img-fluid" src="img/blog/dulichlagi/sanphamdulichj.jpg" style="max-width: 60px" alt="Bài tiếp theo : Sản phẩm du lịch">
+                                        <img class="img-fluid" src="img/post/next.png" alt="">
                                     </a>
                                 </div>
                             </div>
@@ -272,7 +274,7 @@
                 </div>
             </div>
             <div class="col-lg-4">
-                <?php include 'leftcategory.php';?>
+                <?php include 'leftcategory.php'; ?>
             </div>
         </div>
     </div>
